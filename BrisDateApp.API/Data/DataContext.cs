15666b0e1e18974a6aea2ef44b9bde1726a6f4c3
BRICK AGCOPRA <brickagcopra>
2018-12-18 13:30:16 -0500

    initial commit

---
+++ BrisDateApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using BrisDateApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BrisDateApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
